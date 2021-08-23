d6584ce91dfdd624303d4ca20bc504acfe9381de
Berna <bernasurfpix@gmail.com>
2021-08-24 09:29:14 +1000

    tweak carousel and add responsiveness css

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
-    margin:15,
+    margin:8,
     nav:false,
     responsive:{
         0:{
             items:1
         },
+        300:{
+            items:2
+        },
         600:{
-            items:3
+            items:4
         },
         1000:{
-            items:5
+            items:6
         }
     }
 })
